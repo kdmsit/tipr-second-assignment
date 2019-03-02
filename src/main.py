a37f5ce229bd8d23ab6e782fcc1144d5ba7e82b7
KISHALAY DAS <kishalay.msit@gmail.com>
2019-03-03 03:30:33 +0530

    tanh Activation function Commit

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ import time
 
 if __name__ == '__main__':
     path = "/home/kdcse/Documents/Second Semester/TIPR/Assignment-2/tipr-second-assignment"
-    print('Welcome to the world of neural networks!(2-Layer)')
+    print('Welcome to the world of neural networks!(2-Layer-local)')
     imagePixelList = []
     imageLabelList = []
     imagePixelListTest = []
@@ -29,9 +29,9 @@ if __name__ == '__main__':
     traindata, testdata, trainlabel, testlabel = train_test_split(imagePixelList, imageLabelList, test_size=0.05,
                                                                   random_state=42)
     model={}
-    learning_rate_list = [0.001,0.002,0.003,0.004,0.005,0.9,1]
-    epoc = 20
-    batchsize = 1000
+    learning_rate_list = [0.001,0.002,0.003,0.004,0.005,0.006,0.007,0.008,0.009]
+    epoc = 50
+    batchsize = 500
     print("Epoc :", epoc)
     for learningrate in learning_rate_list:
         learning_rate = learningrate
@@ -52,7 +52,7 @@ if __name__ == '__main__':
                     y.append(labellist)
                 Y = np.asarray(y, dtype=None, order=None)
                 if(k==0):
-                    model = nn.initialize_parameters(X, 1500,100,10)
+                    model = nn.initialize_parameters(X, 600,50,10)
                 model = nn.train(model, X, Y,learning_rate)
                 batchstartIndex=batchendIndex
                 batchendIndex=batchstartIndex+batchsize
--- src/nn.py
+++ src/nn.py
@@ -21,9 +21,15 @@ def tanh_derivative(x):
 def relu(z):
     return np.maximum(z, 0)
 
-
 def relu_derivative(z):
     return 1. * (z > 0)
+
+def softmax(z):
+    return np.exp(z) / np.sum(np.exp(z))
+
+
+def softmax_derivative(z):
+    return softmax(z) * (1 - softmax(z))
 
 def initialize_parameters(X,h1,h2,out):
     model = {}
@@ -35,19 +41,25 @@ def initialize_parameters(X,h1,h2,out):
 
 def forward_prop(X,model):
     weights1, weights2,weights3 = model['weights1'], model['weights2'],model['weights3']
-    layer1 = sigmoid(np.dot(X, weights1))
-    layer2 = sigmoid(np.dot(layer1, weights2))
-    output = sigmoid(np.dot(layer2, weights3))
+    #layer1 = sigmoid(np.dot(X, weights1))
+    layer1 = tanh(np.dot(X, weights1))
+    #layer2 = sigmoid(np.dot(layer1, weights2))
+    layer2 = tanh(np.dot(layer1, weights2))
+    #output = sigmoid(np.dot(layer2, weights3))
+    output = softmax(np.dot(layer2, weights3))
     model['output'] = output
     return output,layer2,layer1, model
 
 def back_prop(X,Y,model,layer1,layer2,learning_rate):
     weights1, weights2,weights3,output = model['weights1'], model['weights2'],model['weights3'], model['output']
-    delta4 = 2 * (Y - output) * sigmoid_derivative(output)
+    #delta4 = 2 * (Y - output) * sigmoid_derivative(output)
+    delta4 = 2 * (Y - output) * softmax_derivative(output)
     d_weights3 = np.dot(layer2.T, delta4)
-    delta3 = np.dot(delta4, weights3.T) * sigmoid_derivative(layer2)
+    #delta3 = np.dot(delta4, weights3.T) * sigmoid_derivative(layer2)
+    delta3 = np.dot(delta4, weights3.T) * tanh_derivative(layer2)
     d_weights2 = np.dot(layer1.T, delta3)
-    delta2 = np.dot(delta3, weights2.T) * sigmoid_derivative(layer1)
+    #delta2 = np.dot(delta3, weights2.T) * sigmoid_derivative(layer1)
+    delta2 = np.dot(delta3, weights2.T) * tanh_derivative(layer1)
     d_weights1 = np.dot(X.T, delta2)
     model['weights3'] += learning_rate * d_weights3
     model['weights1'] += learning_rate * d_weights1
